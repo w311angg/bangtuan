88db0a9dd2f9bc096de40172cb66fbf985f770fb
w311ang <30381083+w311ang@users.noreply.github.com>
2022-07-13 00:28:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from markdown import markdown
 from pytools.pytools import jmail
 import os
 
-s=httpx.Client(verify=False)
+s=httpx.Client()
 logindata=os.getenv('logindata')
 
 idlist=pickleread('idlist.txt',[])
@@ -17,11 +17,12 @@ for i in tasklist:
   id=i['earnid']
   name=i['prizename']
   count=i['remaincount']
-  if not id in idlist:
-    idlist.append(id)
+  if (not id in idlist) or on!='schedule':
+    if on=='schedule':
+      idlist.append(id)
     freshtasks.append({'name':name,'count':count})
 
-for count, i in enumerate(freshtasks):
+for count, i in enumerate(sorted(freshtasks,key=lambda d: d['count'])):
   md+='%s* %s (剩%s名额)'%('\n' if count>0 else '',i['name'],i['count'])
 mailhtml=markdown(md)
 print(md)
