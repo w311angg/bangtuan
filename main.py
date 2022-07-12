ac8a164da2637b068700ce523306e9a61440158b
w311ang <30381083+w311ang@users.noreply.github.com>
2022-07-12 16:57:19 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import httpx
+from pytools.pytools import pickledump, pickleread
+from markdown import markdown
+from pytools.pytools import jmail
+import os
+
+s=httpx.Client(verify=False)
+logindata=os.getenv('logindata')
+
+idlist=pickleread('idlist.txt',[])
+freshtasks=[]
+md=''
+
+resp=s.post('https://c.29592.net/api/EarnUser/BangBangTuanV2',data=logindata,headers={'content-type':'application/json; charset=utf-8'}).json()
+tasklist=resp['tasklist']
+for i in tasklist:
+  id=i['earnid']
+  name=i['prizename']
+  count=i['remaincount']
+  if not id in idlist:
+    idlist.append(id)
+    freshtasks.append({'name':name,'count':count})
+
+pickledump(idlist,'idlist.txt')
+
+for count, i in enumerate(freshtasks):
+  md+='%s* %s (剩%s名额)'%('\n' if count>0 else '',i['name'],i['count'])
+mailhtml=markdown(md)
+
+jmail('帮帮团上了%s个新任务'%len(freshtasks),mailhtml,html=True)
+print(md)
+print(mailhtml)
